882c32ab2f3b1565a017cba91d6923737255ba70
Millena Limaverde <millenalimaverde@outlook.com>
2024-11-28 23:06:51 -0300

    test: 💍 adjusting features and testing new trips

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,20 +15,53 @@ export default function HomePage() {
   };
 
   return (
+  <>
     <div style={{ textAlign: 'center', padding: '50px' }}>
       <h1>Planejador de Viagens</h1>
-      <input
-        type="text"
-        placeholder="Nome da Viagem"
-        value={tripName}
+        <div>
+         <h3>Crie uma Nova Viagem</h3>
+          <input
+          type="text"
+          placeholder="Nome da Viagem"
+          value={tripName}
+          onChange={(e) => setTripName(e.target.value)}
+          style={{ padding: '10px', fontSize: '16px', marginBottom: '20px' }}
+          />
+         <br />
+        </div>
+        <div>
+        <h3>Tempo da Viagem</h3>
+        <label>Data de Ida: </label>
+        <input
+          type="date"
+          style={{ padding: '10px', fontSize: '16px', marginBottom: '20px' }}
+        />
+        <label>Data de Retorno: </label>
+        <input
+          type="date"
+          style={{ padding: '10px', fontSize: '16px', marginBottom: '20px' }}
+        />
+        
 
-        onChange={(e) => setTripName(e.target.value)}
-        style={{ padding: '10px', fontSize: '16px', marginBottom: '20px' }}
-      />
-      <br />
+        </div>
+      
+
+
       <button onClick={handleCreateTrip} style={{ padding: '10px 20px', fontSize: '16px' }}>
         Criar Viagem
       </button>
     </div>
+
+    <div>
+      <h2>Viagens Criadas</h2>
+      <ul>
+        {trip.map((trip) => (
+          <li key={trip.id}>
+            <a href={`/trip/${trip.id}`}>{trip.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </>
   );
 }
